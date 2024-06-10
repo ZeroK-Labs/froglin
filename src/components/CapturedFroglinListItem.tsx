85cb5da5d2f98c05bf4cadd34878d06b3ed94a6c
grasshopper47 <petrut.suciu@gmail.com>
2024-06-10 10:13:08 +0200

    fix border on capture list item

--- src/components/CapturedFroglinListItem.tsx
+++ src/components/CapturedFroglinListItem.tsx
@@ -2,7 +2,10 @@ import { Froglin } from "types";
 
 export default function CapturedFroglinListItem({ item }: { item: Froglin }) {
   return (
-    <div className="p-2 border-[3px] bg-main-purple border-main-purple-hover border-dashed">
+    <div
+      className="p-2 border-[3px] bg-main-purple border-dashed"
+      style={{ borderColor: "rgba(102, 34, 155, 0.8)" }}
+    >
       <img
         src={`/images/froglin${item.type}.png`}
         width="36px"
