190a36a2da2294a36ad2fcd02b0d830626eb9dcb
grasshopper47 <petrut.suciu@gmail.com>
2024-07-04 20:34:13 -0700

    fix sed of nargo.toml

--- scripts/prerequisites/_/aztec-sandbox.sh
+++ scripts/prerequisites/_/aztec-sandbox.sh
@@ -7,27 +7,28 @@ get_version_aztec-sandbox() {
   docker compose -f $HOME/.aztec/docker-compose.yml -p sandbox down > /dev/null 2>&1
 
   # read the Node.js version from .nvmrc
-  local PROJECT_VERSION=$(aztec --version | grep -oE "[0-9]+\.[0-9]+\.[0-9]+" 2>/dev/null)
+  local INSTALL_VERSION=$(aztec --version 2>/dev/null | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
 
   # grep the line containing "@aztec/accounts"
   local line=$(grep '@aztec/aztec.js' package.json)
 
+  # extract the version number
+  local PACKAGE_VERSION=$(echo $line | sed -n 's/.*"@aztec\/aztec\.js": "\([0-9.]*\)".*/\1/p')
+
   # extract the toml file version
   local TOML_VERSION=$(grep -oE 'aztec-packages-v[0-9]+\.[0-9]+\.[0-9]+' src/contracts/Nargo.toml | head -n 1 | sed -E 's/aztec-packages-v([0-9]+\.[0-9]+\.[0-9]+)/\1/')
 
-  # sync toml version with project version when different
-  if [[ "$PROJECT_VERSION" != "$TOML_VERSION" ]]; then
-    sed -i "aztec-packages-v$TOML_VERSION" "aztec-packages-v$PROJECT_VERSION/" src/contracts/Nargo.toml
+  # sync toml version with package version when different
+  if [[ "$PACKAGE_VERSION" != "$TOML_VERSION" ]]; then
+    sed -i "s/aztec-packages-v$TOML_VERSION/aztec-packages-v$PACKAGE_VERSION/g" src/contracts/Nargo.toml
   fi
 
-  # extract the version number
-  local INSTALLED_VERSION=$(echo $line | sed -n 's/.*"@aztec\/aztec\.js": "\([0-9.]*\)".*/\1/p')
-
-  if [[ "$PROJECT_VERSION" == "$INSTALLED_VERSION" ]]; then
-    echo $PROJECT_VERSION
+  if [[ "$PACKAGE_VERSION" == "$INSTALL_VERSION" ]]; then
+    echo $PACKAGE_VERSION
   else
     return 1
   fi
+
 }
 
 install_aztec-sandbox() {
