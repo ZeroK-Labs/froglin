8f7476657a6dce0dea2ef09012556fce096adf42
grasshopper47 <petrut.suciu@gmail.com>
2024-07-04 20:34:02 -0700

    remove extra source

--- scripts/prerequisites/_/bun.sh
+++ scripts/prerequisites/_/bun.sh
@@ -12,9 +12,6 @@ install_bun() {
     echo "# bun" >> $ENV_VARS_FILE
     echo "export BUN_INSTALL="$HOME/.bun"" >> $ENV_VARS_FILE
     echo "export PATH=$BUN_INSTALL/bin:\$PATH" >> $ENV_VARS_FILE
-
-    # reload bash context
-    source $ENV_VARS_FILE
   fi
 
   # reload bash context
