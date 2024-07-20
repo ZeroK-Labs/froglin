2395741d821023cf1311182611710749c662692c
grasshopper47 <petrut.suciu@gmail.com>
2024-07-20 23:52:27 +0200

    move ip utils from common to PXEManager

--- common/PXEManager.ts
+++ common/PXEManager.ts
@@ -1,6 +1,22 @@
+import os from "os";
 import { ChildProcess, spawn } from "child_process";
 
-import { getLocalIP } from "../common/IPUtils";
+// TODO: cannot access sandbox using localhost, need IP (should be SANDBOX_HOST)
+export function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  if (!interfaces) throw "This device is missing a network adapter";
+
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]!) {
+      // Skip over internal (i.e., 127.0.0.1) and non-IPv4 addresses
+      if ("IPv4" !== iface.family || iface.internal !== false) continue;
+
+      return iface.address;
+    }
+  }
+
+  throw "Failed to find IP address of this device";
+}
 
 const PXE_PORT_LOWER_BOUND = Number(process.env.SANDBOX_PORT) + 1;
 const PXE_PORT_UPPER_BOUND = 65535;
