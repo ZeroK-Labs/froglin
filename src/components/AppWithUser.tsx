ee5223d0973c95b246be63f904f5bbb3e0f0cef5
grasshopper47 <petrut.suciu@gmail.com>
2024-07-24 18:54:26 +0200

    add pxe client back

--- src/components/AppWithUser.tsx
+++ src/components/AppWithUser.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
-import { GameEventProvider } from "stores";
+import { useEffect, useState } from "react";
+import { SingleKeyAccountContract } from "@aztec/accounts/single_key";
+import {
+  Fr,
+  deriveMasterIncomingViewingSecretKey,
+  AccountManager,
+} from "@aztec/aztec.js";
 
+import { GameEventProvider, usePXEClient } from "stores";
 import { useTutorialState, useViewState } from "hooks";
-
 import {
   CapturedFroglinList,
   InfoBarsContainer,
@@ -26,6 +31,33 @@ function AppWithUser() {
 
   const { tutorial, setTutorial } = useTutorialState();
   const { view, setView } = useViewState();
+  const { pxeClient } = usePXEClient();
+
+  useEffect(
+    () => {
+      if (!pxeClient) return;
+
+      async function registerAccount() {
+        if (!pxeClient) return;
+
+        const secretString = localStorage.getItem("user");
+        if (!secretString) {
+          throw "Failed to get secret: key 'user' missing from localStorage";
+        }
+
+        const keyFr = new Fr(BigInt(secretString));
+        const encryptionPrivateKey = deriveMasterIncomingViewingSecretKey(keyFr);
+
+        const accountContract = new SingleKeyAccountContract(encryptionPrivateKey);
+        const accountManager = new AccountManager(pxeClient, keyFr, accountContract);
+
+        await accountManager.register();
+      }
+
+      registerAccount();
+    }, //
+    [pxeClient],
+  );
 
   return (
     <>
