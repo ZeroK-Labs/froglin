08ab88d5401a25a5319e73f52a99f043cca50c54
grasshopper47 <petrut.suciu@gmail.com>
2024-07-26 16:06:11 +0200

    update wallet generation to create same address

--- common/WalletManager.ts
+++ common/WalletManager.ts
@@ -1,4 +1,7 @@
 import { AccountManager } from "@aztec/aztec.js/account";
+// TODO: why TX dropped by P2P node when using Schnorr directly?
+// import { SchnorrAccountContract } from "@aztec/accounts/schnorr";
+
 import { SingleKeyAccountContract } from "@aztec/accounts/single_key";
 import {
   AccountWallet,
@@ -7,14 +10,14 @@ import {
   deriveMasterIncomingViewingSecretKey,
 } from "@aztec/aztec.js";
 
-type SecretKey = number | bigint | boolean | Fr | Buffer;
+export async function createWallet(secret: string, pxe: PXE): Promise<AccountWallet> {
+  const bigNumber = BigInt(secret);
+  const secretKey = new Fr(bigNumber);
+  const signingPrivateKey = deriveMasterIncomingViewingSecretKey(secretKey);
+  const contract = new SingleKeyAccountContract(signingPrivateKey);
+  const account = new AccountManager(pxe, secretKey, contract, Fr.ZERO);
 
-export async function createWallet(secret: SecretKey, pxe: PXE): Promise<AccountWallet> {
-  const secretKey = new Fr(secret);
-  const encryptionPrivateKey = deriveMasterIncomingViewingSecretKey(secretKey);
-  const contract = new SingleKeyAccountContract(encryptionPrivateKey);
-  const manager = new AccountManager(pxe, secretKey, contract);
-
-  const wallet = await manager.register();
+  const wallet = await account.waitSetup();
+  console.log("wallet ready", wallet.getAddress().toString());
   return wallet;
 }
