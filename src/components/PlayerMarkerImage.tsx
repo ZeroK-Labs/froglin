200c1875844280f8cc20ba0f7372c77d0b1e61ce
grasshopper47 <petrut.suciu@gmail.com>
2024-07-29 23:05:59 +0200

    update display

--- src/components/PlayerMarkerImage.tsx
+++ src/components/PlayerMarkerImage.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function PlayerMarkerImage({ size, grayscale = false }: Props) {
   return (
     <img
-      className={`relative self-center border-2 rounded-full border-gray-800 border-solid ${grayscale ? "filter grayscale-80 brightness-90" : ""}`}
+      className={`inline-block border-2 rounded-full border-solid border-gray-800 ${grayscale ? "filter grayscale-80 brightness-90" : ""}`}
       src="/images/avatar.webp"
       width={size}
       height={size}
