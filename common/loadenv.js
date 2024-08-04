76861e8527adb5ffba54597420b7b29873134f4c
grasshopper47 <petrut.suciu@gmail.com>
2024-08-04 21:36:18 +0200

    remove BACKEND_PROTOCOL

--- common/loadenv.js
+++ common/loadenv.js
@@ -21,12 +21,7 @@ async function loadenv() {
 
   process.env.SANDBOX_URL = process.env.SANDBOX_ROOT + configuration.SANDBOX_PORT;
 
-  process.env.BACKEND_URL =
-    process.env.BACKEND_PROTOCOL +
-    "://" +
-    process.env.BACKEND_HOST +
-    ":" +
-    process.env.BACKEND_PORT;
+  process.env.BACKEND_URL = `https://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}`;
 
   process.env.WSS_URL = `wss://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}`;
 }
