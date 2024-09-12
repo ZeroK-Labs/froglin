988b29586bccc7eee499eeced52fe4a16a680a20
vladandrei <vlad.andrei@flip.ro>
2024-09-12 16:27:12 +0100

    removed leaderboard from endpoints

--- backend/endpoints/index.ts
+++ backend/endpoints/index.ts
@@ -1,6 +1,5 @@
 export { getEventBounds } from "./EventBounds";
 export { getGatewayAddress } from "./gateway";
 export { getInterestPoints } from "./InterestPoints";
-export { getLeaderboard } from "./leaderboard";
 export { revealFroglins } from "./reveal";
 export { setPlayerLocation } from "./PlayerLocation";
