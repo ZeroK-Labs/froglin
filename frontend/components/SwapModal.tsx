bbaa4d38898655eeb21a3a54350725f241d29331
grasshopper47 <petrut.suciu@gmail.com>
2024-10-12 17:17:59 +0200

    fix hide on create; fix fade on show; minor refactor

--- frontend/components/SwapModal.tsx
+++ frontend/components/SwapModal.tsx
@@ -1,22 +1,19 @@
-import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
+import { useState, useEffect } from "react";
 
 import { MODALS } from "frontend/enums";
 import { Modal } from "frontend/components";
-import { useGameEvent, useModalState, usePlayer } from "frontend/stores";
 import { names } from "frontend/components/FroglinModal";
+import { useGameEvent, useModalState, usePlayer } from "frontend/stores";
 
 export default function SwapModal() {
-  const { selectedFroglin } = useGameEvent();
-  const { modal, setModal } = useModalState();
-  const { aztec, registered } = usePlayer();
   const [enemyFroglin, setEnemyFroglin] = useState<number | null>(null);
 
-  const visible = modal === MODALS.SWAP;
+  const { selectedFroglin } = useGameEvent();
+  const { modal, setModal } = useModalState();
+  const { aztec } = usePlayer();
 
-  useEffect(() => {
-    setEnemyFroglin(null);
-  }, [modal]);
+  const visible = modal === MODALS.SWAP;
 
   function changeFroglin() {
     setEnemyFroglin((prev) => {
@@ -29,23 +26,34 @@ export default function SwapModal() {
       return (prev + 1) % names.length;
     });
   }
+
   async function createSwapOffer() {
-    if (!aztec || !registered) return;
+    if (!aztec) return;
     if (selectedFroglin === null || enemyFroglin === null) return;
+
+    setModal(MODALS.NONE);
+
     const toastId = toast.loading("Creating swap offer...");
     await aztec.contracts.gateway.methods
       .create_swap_proposal(selectedFroglin, enemyFroglin)
       .send()
       .wait();
     toast.success("Swap offer created!", { id: toastId });
-    setModal(MODALS.NONE);
   }
+
+  useEffect(
+    () => {
+      if (visible) setEnemyFroglin(null);
+    }, //
+    [visible],
+  );
 
   if (selectedFroglin === null) return null;
 
   return (
     <Modal
       className="top-4"
+      icon="🔄"
       title="Swap"
       visible={visible}
     >
@@ -53,37 +61,37 @@ export default function SwapModal() {
         <div className="flex flex-row w-full justify-between items-center my-6 relative gap-8">
           <div>
             {selectedFroglin !== null ? (
-              <img
-                src={`/images/froglin${selectedFroglin}-large.webp`}
-                width="160px"
-                height="160px"
-                alt="froglin"
-              />
+              <>
+                <img
+                  src={`/images/froglin${selectedFroglin}-large.webp`}
+                  width="160px"
+                  height="160px"
+                  alt="froglin"
+                />
+                <span className="pb-6">{names[selectedFroglin][0]}</span>
+              </>
             ) : null}
-            <span className="pb-6">
-              {selectedFroglin !== null && names[selectedFroglin][0]}
-            </span>
           </div>
 
           <div onClick={changeFroglin}>
             {enemyFroglin !== null ? (
-              <img
-                src={`/images/froglin${enemyFroglin}-large.webp`}
-                width="160px"
-                height="160px"
-                alt="froglin"
-              />
+              <>
+                <img
+                  src={`/images/froglin${enemyFroglin}-large.webp`}
+                  width="160px"
+                  height="160px"
+                  alt="froglin"
+                />
+                <span className="pb-6 min-h-3">{names[enemyFroglin][0]}</span>
+              </>
             ) : (
               <>
-                <div className="w-[160px] h-[160px] border-2 border-white flex items-center justify-center">
-                  Select Froglin to swap for
+                <div className="w-[160px] h-[160px] p-2 border-2 border-white flex items-center justify-center">
+                  Tap here to select a Froglin to swap for
                 </div>
                 <span className="py-6 min-h-3">???</span>
               </>
             )}
-            <span className="py-6 min-h-3">
-              {enemyFroglin !== null ? names[enemyFroglin][0] : ""}
-            </span>
           </div>
         </div>
 
