6e8896ca688c86f79bf8a7b51f6dd1b8757534c7
vladandrei <vlad.andrei@flip.ro>
2024-10-15 17:02:03 +0100

    claim

--- frontend/components/ClaimsModal.tsx
+++ frontend/components/ClaimsModal.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import toast from "react-hot-toast";
 import type { SwapOfferResponse, SwapOffer } from "frontend/types";
 import { MODALS } from "frontend/enums";
 import { Modal } from "frontend/components";
@@ -8,12 +8,11 @@ import { useModalState, usePlayer } from "frontend/stores";
 
 export default function ClaimsModal() {
   const [claims, setClaims] = useState<SwapOffer[]>([]);
-
+  const [refetch, setRefetch] = useState<boolean>(false);
   const { modal } = useModalState();
   const { aztec, registered, traderId } = usePlayer();
 
   const visible = modal === MODALS.CLAIMS;
-  console.log("claims", claims);
 
   useEffect(
     () => {
@@ -21,13 +20,10 @@ export default function ClaimsModal() {
         if (!aztec || !registered || !visible || !traderId) return;
 
         const claimsResponse: SwapOfferResponse[] =
-          await aztec.contracts.gateway.methods.view_all_proposals().simulate();
+          await aztec.contracts.gateway.methods
+            .view_claimable_swaps(traderId)
+            .simulate();
 
-        console.log("claims", claimsResponse);
-        const res = await aztec.contracts.gateway.methods
-          .view_claimable_swaps(traderId)
-          .simulate();
-        console.log("res", res);
         if (!claimsResponse || claimsResponse.length === 0) return;
 
         const numberList: SwapOffer[] = [];
@@ -50,8 +46,28 @@ export default function ClaimsModal() {
 
       fetchClaims();
     }, //
-    [aztec, registered, visible],
+    [aztec, registered, visible, refetch],
   );
+
+  async function handleClaim(proposalId: number) {
+    if (!aztec || !registered) return;
+
+    const toastId = toast.loading("Claiming swap offer...");
+
+    try {
+      await aztec.contracts.gateway.methods
+        .claim_swap_proposal(proposalId)
+        .send()
+        .wait();
+
+      setRefetch(!refetch);
+    } catch (error) {
+      console.error("Error claiming swap:", error);
+      toast.error("Failed to claim swap!", { id: toastId });
+    }
+
+    toast.success("Claimed!", { id: toastId });
+  }
 
   return (
     <Modal
@@ -60,6 +76,30 @@ export default function ClaimsModal() {
       visible={visible}
     >
       <div className="flex flex-col">
+        {claims.map((claim) => (
+          <div
+            key={claim.id}
+            className="w-[299px] h-[40px] flex items-center justify-between bg-gray-300 font-extrabold text-gray-900 rounded-md mb-2"
+            onClick={() => handleClaim(claim.id)}
+          >
+            <div className="flex flex-row items-center gap-2">
+              <img
+                src={`/images/froglin${claim.wanted_froglin_type}.webp`}
+                alt="Left Image"
+                width="40px"
+                height="40px"
+                className="rounded-md"
+              />
+              <span className="text-sm no-text-shadow">
+                {names[claim.wanted_froglin_type][0]}
+              </span>
+            </div>
+
+            <span className="text-sm font-semibold no-text-shadow mr-4">
+              Won in battle
+            </span>
+          </div>
+        ))}
         <div className="w-[299px] h-[40px] flex items-center justify-between bg-gray-300 font-extrabold text-gray-900 rounded-md mb-2">
           <div className="flex flex-row items-center gap-2">
             <img
