0f482a880edaa61d5e11882e198da0eb9d6b905b
vladandrei <vlad.andrei@flip.ro>
2024-12-05 19:07:53 +0000

    accept battle wip

--- frontend/components/NoticeBoardModal.tsx
+++ frontend/components/NoticeBoardModal.tsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from "react";
 
 import type { SwapOfferResponse, Proposal } from "frontend/types";
 import { MODALS } from "frontend/enums";
-import { Modal } from "frontend/components";
+import { BattleOptionBox, Modal } from "frontend/components";
 import { names } from "frontend/components/FroglinModal";
 import { useModalState, usePlayer } from "frontend/stores";
+import { FroglinMenuButton } from "./FroglinMenuButton";
 
 export default function NoticeBoardModal() {
   const [offers, setOffers] = useState<Proposal[]>([]);
   const [refetch, setRefetch] = useState<boolean>(true);
+  const [choices, setChoices] = useState<number[]>([0, 0, 0]);
+  const [makeChoices, setMakeChoices] = useState<boolean>(false);
 
   const { modal } = useModalState();
   const { aztec, registered, traderId, stash, fetchStash } = usePlayer();
@@ -44,6 +47,26 @@ export default function NoticeBoardModal() {
     toast.dismiss(toastId);
   }
 
+  async function acceptBattle(proposalId: number) {
+    if (!aztec || !registered) return;
+
+    const toastId = toast.loading("Accepting battle offer...");
+    const battleNumber = choices.reduce((acc, choice) => acc * 10 + choice, 0);
+    try {
+      await aztec.contracts.gateway.methods
+        .accept_battle_proposal(proposalId, battleNumber)
+        .send()
+        .wait();
+
+      setRefetch(true);
+      setMakeChoices(false);
+    } catch (error) {
+      console.error("Error accepting battle offer:", error);
+      toast.error("Failed to accept battle offer!", { id: toastId });
+    }
+
+    toast.dismiss(toastId);
+  }
   async function handleAccept(proposalId: number, type: string) {
     if (!aztec || !registered) return;
 
@@ -51,11 +74,8 @@ export default function NoticeBoardModal() {
 
     try {
       if (type === "battle") {
-        // open battle modal
-        // await aztec.contracts.gateway.methods
-        //   .accept_battle_proposal(proposalId)
-        //   .send()
-        //   .wait();
+        // render choices
+        setMakeChoices(true);
       }
       if (type === "swap") {
         await aztec.contracts.gateway.methods
@@ -149,126 +169,98 @@ export default function NoticeBoardModal() {
           : offers.map((offer) => {
               // console.log(offer, traderId, offer.trader_id === traderId);
               return (
-                <div
-                  key={offer.id}
-                  className="mb-2 flex flex-row"
-                >
-                  <div className="h-[32px] rounded-md font-extrabold flex align-middle self-center items-center justify-between bg-gray-300 text-gray-900">
-                    <img
-                      src={`/images/froglin${offer.offered_froglin_type}.webp`}
-                      alt="Left Image"
-                      width="40px"
-                      height="40px"
-                      className="rounded-md"
-                    />
+                <div>
+                  <div
+                    key={offer.id}
+                    className="mb-2 flex flex-row"
+                  >
+                    <div className="h-[32px] rounded-md font-extrabold flex align-middle self-center items-center justify-between bg-gray-300 text-gray-900">
+                      <img
+                        src={`/images/froglin${offer.offered_froglin_type}.webp`}
+                        alt="Left Image"
+                        width="40px"
+                        height="40px"
+                        className="rounded-md"
+                      />
 
-                    <span className="w-24 text-md no-text-shadow">
-                      {names[offer.offered_froglin_type][0]}
-                    </span>
-                    <span className="text-xl">{offerType(offer.type)}</span>
-                    <span className="w-24 text-md no-text-shadow">
-                      {names[offer.wanted_froglin_type][0]}
-                    </span>
+                      <span className="w-24 text-md no-text-shadow">
+                        {names[offer.offered_froglin_type][0]}
+                      </span>
+                      <span className="text-xl">{offerType(offer.type)}</span>
+                      <span className="w-24 text-md no-text-shadow">
+                        {names[offer.wanted_froglin_type][0]}
+                      </span>
 
-                    <img
-                      src={`/images/froglin${offer.wanted_froglin_type}.webp`}
-                      alt="Right Image"
-                      width="40px"
-                      height="40px"
-                      className="rounded-md"
-                    />
+                      <img
+                        src={`/images/froglin${offer.wanted_froglin_type}.webp`}
+                        alt="Right Image"
+                        width="40px"
+                        height="40px"
+                        className="rounded-md"
+                      />
+                    </div>
+
+                    {offer.trader_id === traderId ? (
+                      <button
+                        type="button"
+                        className="rounded-lg px-2 py-1 ml-2 my-2 text-xs font-semibold shadow-sm text-white bg-red-800"
+                        onClick={() => handleCancel(offer.id, offer.type)}
+                      >
+                        Cancel
+                      </button>
+                    ) : (
+                      <button
+                        disabled={stash[offer.wanted_froglin_type] === 0}
+                        type="button"
+                        className={`rounded-lg px-2 py-1 ml-2 my-2 text-xs font-semibold shadow-sm text-white bg-green-600 ${stash[offer.wanted_froglin_type] === 0 ? "grayscale" : ""}`}
+                        onClick={() => handleAccept(offer.id, offer.type)}
+                      >
+                        Accept
+                      </button>
+                    )}
                   </div>
-
-                  {offer.trader_id === traderId ? (
-                    <button
-                      type="button"
-                      className="rounded-lg px-2 py-1 ml-2 my-2 text-xs font-semibold shadow-sm text-white bg-red-800"
-                      onClick={() => handleCancel(offer.id, offer.type)}
-                    >
-                      Cancel
-                    </button>
-                  ) : (
-                    <button
-                      disabled={stash[offer.wanted_froglin_type] === 0}
-                      type="button"
-                      className={`rounded-lg px-2 py-1 ml-2 my-2 text-xs font-semibold shadow-sm text-white bg-green-600 ${stash[offer.wanted_froglin_type] === 0 ? "grayscale" : ""}`}
-                      onClick={() => handleAccept(offer.id)}
-                    >
-                      Accept
-                    </button>
-                  )}
+                  {makeChoices ? (
+                    <div>
+                      <div className="flex flex-row justify-between items-center gap-4 pb-8">
+                        <div>
+                          <span className="text-center">Round 1</span>
+                          <BattleOptionBox
+                            box={1}
+                            setChoices={setChoices}
+                            choices={choices}
+                            currentOption={choices[0] ?? ""}
+                          />
+                        </div>
+                        <div>
+                          <span className="text-center">Round 2</span>
+                          <BattleOptionBox
+                            box={2}
+                            setChoices={setChoices}
+                            choices={choices}
+                            currentOption={choices[1] ?? ""}
+                          />
+                        </div>
+                        <div>
+                          <span className="text-center">Round 3</span>
+                          <BattleOptionBox
+                            box={3}
+                            setChoices={setChoices}
+                            choices={choices}
+                            currentOption={choices[2] ?? ""}
+                          />
+                        </div>
+                      </div>
+                      <FroglinMenuButton
+                        className="bg-gray-900"
+                        icon="🗡️"
+                        text="Send to Battle"
+                        onClick={acceptBattle(offer.id)}
+                      />
+                    </div>
+                  ) : null}
                 </div>
               );
             })}
-        {/* <div
-          className="w-[299px] h-[40px]
-          flex items-center justify-between
-        bg-gray-300 font-extrabold text-gray-900 rounded-md mb-2"
-        >
-          <img
-            src="/images/froglin1.webp"
-            alt="Left Image"
-            width="40px"
-            height="40px"
-            className="rounded-md"
-          />
-          <span className="text-md no-text-shadow">{names[1][0]}</span>
-          <span className="text-4xl">🗡️</span>
-          <span className="text-md no-text-shadow">{names[2][0]}</span>
-          <img
-            src="/images/froglin2.webp"
-            alt="Right Image"
-            width="40px"
-            height="40px"
-            className="rounded-md"
-          />
-        </div>
-        <div
-          className="w-[299px] h-[40px]
-          flex items-center justify-between
-        bg-blue-300 font-extrabold text-gray-900 rounded-md mb-2"
-        >
-          <img
-            src="/images/froglin1.webp"
-            alt="Left Image"
-            width="40px"
-            height="40px"
-            className="rounded-md"
-          />
-          <span className="text-md no-text-shadow">{names[1][0]}</span>
-          <span className="text-4xl">🔄</span>
-          <span className="text-md no-text-shadow">{names[2][0]}</span>
-          <img
-            src="/images/froglin2.webp"
-            alt="Right Image"
-            width="40px"
-            height="40px"
-            className="rounded-md"
-          />
-        </div>
-        <div
-          className="w-[299px] h-[40px]
-          flex items-center justify-between
-        bg-red-300 font-extrabold text-gray-900 rounded-md mb-2"
-        >
-          <img
-            src="/images/froglin1.webp"
-            alt="Left Image"
-            width="40px"
-            height="40px"
-            className="rounded-md"
-          />
-          <span className="text-md no-text-shadow">{names[1][0]}</span>
-          <span className="text-4xl">❤️</span>
-          <span className="text-md no-text-shadow">{names[2][0]}</span>
-          <img
-            src="/images/froglin2.webp"
-            alt="Right Image"
-            width="40px"
-            height="40px"
-            className="rounded-md"
-          />
-        </div> */}
       </div>
     </Modal>
   );
