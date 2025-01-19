467c5b315d6b1582540610d2117233726e784ae4
grasshopper47 <petrut.suciu@gmail.com>
2025-01-19 23:16:00 +0000

    add join/leave event UI

--- frontend/components/WorldEventMarker.tsx
+++ frontend/components/WorldEventMarker.tsx
@@ -3,6 +3,7 @@ import { Marker } from "react-map-gl";
 
 import type { WorldEvent } from "common/types";
 import { FROGLIN, VIEW } from "frontend/settings";
+import { usePlayer } from "frontend/stores";
 
 type Props = {
   visible: boolean;
@@ -11,15 +12,39 @@ type Props = {
 
 export default function WorldEventMarker(props: Props) {
   const locationRef = useRef(props.event.coordinates);
+  const showTimerRef = useRef<Timer>();
 
   const [opacity, setOpacity] = useState(0);
   const [message, setMessage] = useState<string>("");
 
+  const { events, joinEvent, leaveEvent } = usePlayer();
+
+  const joined = events.some((e) => e === props.event);
+
+  function hide() {
+    clearTimeout(showTimerRef.current);
+    showTimerRef.current = setTimeout(setMessage, FROGLIN.MARKER.MESSAGE_TIMEOUT, "");
+  }
+
+  function keep() {
+    clearTimeout(showTimerRef.current);
+  }
+
   function handlePinClick(ev: React.MouseEvent) {
     setMessage(props.event.name);
-    setTimeout(setMessage, FROGLIN.MARKER.MESSAGE_TIMEOUT, "");
+    hide();
 
     ev.stopPropagation();
+  }
+
+  function handleJoinEvent() {
+    joinEvent(props.event);
+    hide();
+  }
+
+  function handleLeaveEvent() {
+    leaveEvent(props.event);
+    hide();
   }
 
   useEffect(
@@ -33,6 +58,7 @@ export default function WorldEventMarker(props: Props) {
       const timer = setTimeout(setOpacity, VIEW.TRANSITION_DURATION - 500, 1);
 
       return () => {
+        clearTimeout(showTimerRef.current);
         clearTimeout(timer);
       };
     }, //
@@ -61,7 +87,13 @@ export default function WorldEventMarker(props: Props) {
         latitude={locationRef.current.lat}
       >
         {message ? (
-          <div className="z-999999 p-0.5 rounded-md text-[12px] leading-3 tracking-wider whitespace-nowrap text-gray-800 shadow-lg">
+          <div
+            className="z-999999 p-0.5 rounded-md text-[12px] leading-3 tracking-wider whitespace-nowrap text-gray-800 shadow-lg"
+            onMouseOver={keep}
+            onMouseOut={hide}
+            onPointerDown={keep}
+            onPointerOut={hide}
+          >
             <div className="max-w-xs mx-auto p-4 rounded-lg bg-green-400">
               <h2 className="mb-2 text-2xl font-semibold">{props.event.name}</h2>
               <p className="mb-2 italic font-semibold text-gray-500">
@@ -73,6 +105,13 @@ export default function WorldEventMarker(props: Props) {
               </div>
               <p className="mt-4">{props.event.coordinates.lng}° longitude</p>
               <p>{props.event.coordinates.lat}° latitude</p>
+
+              <button
+                className={`mt-8 px-3 py-1 rounded text-white text-sm ${joined ? "bg-red-900 hover:bg-red-600" : "bg-green-900 hover:bg-green-600"}`}
+                onClick={joined ? handleLeaveEvent : handleJoinEvent}
+              >
+                {joined ? "Leave" : "Join"} Event
+              </button>
             </div>
           </div>
         ) : (
